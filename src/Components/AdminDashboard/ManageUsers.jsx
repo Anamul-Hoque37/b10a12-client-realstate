a3b2e46bc0c4c037bccea6b8fd1205c57384b3c5
anamul hoque <anamul.eee73@gmail.com>
2025-01-16 15:21:36 +0600

    added DELETE for Manage users

--- src/Components/AdminDashboard/ManageUsers.jsx
+++ src/Components/AdminDashboard/ManageUsers.jsx
@@ -2,16 +2,43 @@ import React from 'react';
 import useAxiosSecure from '../../Hooks/useAxiosSecure';
 import { useQuery } from '@tanstack/react-query';
 import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const ManageUsers = () => {
     const axiosSecure = useAxiosSecure();
-    const {data: users =[]} = useQuery({
+    const {data: users =[], refetch } = useQuery({
         queryKey: ['users'],
         queryFn: async () =>{
             const res = await axiosSecure.get('/users');
             return res.data;
         }
     })
+
+    const handleDelete = id => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.delete(`/users/${id}`)
+                    .then(res => {
+                        if (res.data.deletedCount > 0) {
+                            refetch()
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+            }
+        });
+    }
     return (
         <div className='w-11/12 min-h-screen mx-auto py-8'>
         <div className='flex justify-between'>
@@ -21,10 +48,10 @@ const ManageUsers = () => {
             <table className="min-w-full border-collapse border border-gray-200 shadow-md">
                 <thead>
                     <tr className="bg-gray-100">
-                        <th className="border w-1/6 border-gray-200 px-4 py-2 text-left font-medium text-gray-700">Name</th>
-                        <th className="border w-1/12 border-gray-200 px-4 py-2 text-left font-medium text-gray-700">Email</th>
-                        <th className="border w-1/12 border-gray-200 px-4 py-2 text-left font-medium text-gray-700">Make Admin</th>
-                        <th className="border w-1/12 border-gray-200 px-4 py-2 text-left font-medium text-gray-700">Make Agent</th>
+                        <th className="border w-1/4 border-gray-200 px-4 py-2 text-left font-medium text-gray-700">Name</th>
+                        <th className="border w-1/3 border-gray-200 px-4 py-2 text-left font-medium text-gray-700">Email</th>
+                        <th className="border w-1/6 border-gray-200 px-4 py-2 text-left font-medium text-gray-700">Make Admin</th>
+                        <th className="border w-1/6 border-gray-200 px-4 py-2 text-left font-medium text-gray-700">Make Agent</th>
                         <th className="border w-1/12 border-gray-200 px-4 py-2 text-left font-medium text-gray-700">Delete</th>
                        
                     </tr>
@@ -32,11 +59,11 @@ const ManageUsers = () => {
                 <tbody>
                     {users.map((data) => (
                         <tr key={data._id}>
-                            <td className="border border-gray-200 px-4 py-2">{data.name}</td>
-                            <td className="border border-gray-200 px-4 py-2">{data.email}</td>
-                            <td className="border border-gray-200 px-4 py-2"><Link to={`/update/${data._id}`}><button className="btn btn-primary  bg-gradient-to-l from-slate-700 via-fuchsia-700 to-slate-700 text-white">Make Admin</button></Link></td>
-                            <td className="border border-gray-200 px-4 py-2"><Link to={`/update/${data._id}`}><button className="btn btn-primary  bg-gradient-to-l from-slate-700 via-fuchsia-700 to-slate-700 text-white">Make Agent</button></Link></td>
-                            <td className="border border-gray-200 px-4 py-2"><button onClick={() => handleDelete(data._id)} className="btn btn-primary  bg-gradient-to-l from-slate-700 via-fuchsia-700 to-slate-700 text-white">Delete</button></td>
+                            <td className="border border-gray-200 px-4">{data.name}</td>
+                            <td className="border border-gray-200 px-4">{data.email}</td>
+                            <td className="border border-gray-200 "><Link to={`/update/${data._id}`}><button className="btn btn-primary w-full bg-fuchsia-700 hover:bg-fuchsia-900  text-white">Make Admin</button></Link></td>
+                            <td className="border border-gray-200 "><Link to={`/update/${data._id}`}><button className="btn btn-primary w-full bg-fuchsia-700 hover:bg-fuchsia-900  text-white">Make Agent</button></Link></td>
+                            <td className="border border-gray-200"><button onClick={() => handleDelete(data._id)} className="btn btn-primary w-full bg-fuchsia-700 hover:bg-fuchsia-900 text-white">Delete</button></td>
                         </tr>
                     ))}
                 </tbody>
